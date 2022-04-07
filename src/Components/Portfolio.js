bd23cddee162a20df83140c64e0886b0142199ae
Hashmat Ali <shahhashmatali667@gmail.com>
2022-04-07 21:50:54 +0500

    Revert "update projects"
    This reverts commit 1db9ddd715af81bc88b5a5d6bd8d6f6f7bd0a348.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -21,45 +21,19 @@ class Portfolio extends Component {
         </div>
       );
     });
-    const project1 = this.props.data.project1.map(function (project1) {
-      let projectImage = "images/portfolio/" + project1.image;
-
-      return (
-        <div key={id++} className="columns portfolio-item">
-          <div className="item-wrap">
-            <a href={project1.url} title={project1.title} target={"_blank"}>
-            <img alt={project1.title} src={projectImage} />
-            <div style={{ textAlign: "center" }}>{project1.title}</div>
-            </a>
-          </div>
-        </div>
-      );
-    });
 
     return (
       <section id="portfolio">
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Learning Project.</h1>
+              <h1>Check Out Some of My Works.</h1>
 
               <div
                 id="portfolio-wrapper"
                 className="bgrid-quarters s-bgrid-thirds cf"
               >
                 {projects}
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Latest Project.</h1>
-
-              <div
-                id="portfolio-wrapper"
-                className="bgrid-quarters s-bgrid-thirds cf"
-              >
-                {project1}
               </div>
             </div>
           </div>
